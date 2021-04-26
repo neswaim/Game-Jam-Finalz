8356cbf5b2601e0c0a64ffd99354baa23e684adf
neswaim <neswaim@iu.edu>
2021-04-25 23:58:06 -0400

    End screen and win condition

--- Global.gd
+++ Global.gd
@@ -2,10 +2,16 @@ extends Node
 
 var cash = 0
 var level = 1
+var w = 6500
 
 func _ready():
 	pass
 	
+		
+func _input(_event):
+	if cash == w:
+		var _new_scene = get_tree().change_scene("res://Win.tscn")
+
 func _unhandled_input(event):
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
